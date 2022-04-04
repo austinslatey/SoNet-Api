793249e725e63c9f045bb3fa9d2973aa6ccf7d03
Mr WhiteKidd <austinslater58@gmail.com>
2022-04-03 22:27:20 -0500

    added thoughts to models, routes, and controllers we have a working api with sucessful responses.

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,12 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api');
 
-// add prefix of `/api` to all of the api routes imported from the `api` directory
+// add prefix `/api` to all api routes imported from `api` directory
 router.use('/api', apiRoutes);
 
 
 router.use((req, res) => {
-  res.status(404).send('<h1>Please Check Your Routes!!!!</h1>');
+  res.status(404).send('<h1>Check Your Routes!!!!</h1>');
 });
 
 module.exports = router;
